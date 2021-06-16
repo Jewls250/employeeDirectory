a4c45702665ae4a0f44ff9b4ea56123e8f5644d2
Giuliano Serawop <gserawop13@gmail.com>
2021-06-16 17:07:39 -0600

    created sort and filter methods

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,26 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [userInfo, setUserInfo] = useState([]);
+  const [originalUserInfo, setOriginalUserInfo] = useState([]);
+
+  useEffect(() =>{
+     fetch(
+       "https://randomuser.me/api/?results=15"
+     )
+       .then((response) => response.json())
+       .then((data) => {
+         console.log("testing")
+         setUserInfo(data.results)
+         setOriginalUserInfo(data.results)
+        });
+  }, [])
+  
+  function clicked (){
+    let newInfo = userInfo.sort(( a, b ) => {
+      if ( a.name.first < b.name.first ){
+    return -1;
+      }
+      if ( a.name.first > b.name.first ){
+        return 1;
+      }
+      return 0;
+    });
+    console.log(newInfo)
+    setUserInfo([...newInfo])
+  }
+  
+  function handleFilter(e){
+    console.log(e.target.value)
+
+    let newInfo = originalUserInfo.filter(item => item.email.includes(e.target.value))
 
 
+    setUserInfo([...newInfo]);
+    
+  }
 
-  fetch("https://randomuser.me/api/?results=1")
-    .then((response) => response.json())
-    .then((data) => setUserInfo(data.results));
+ 
 
 
 
   
   return (
-    
     <div className="App">
+      <p>Sort by first name</p>
+      <input onChange={handleFilter} placeholder="Filter Email"/>
       <table>
         <thead>
           <tr>
-            <th>
+            <th onClick={clicked}>
               <p>first Name</p>
             </th>
             <th>
@@ -32,33 +66,23 @@ function App() {
             <th>
               <p>Location</p>
             </th>
+            <th>
+              <p>Email</p>
+            </th>
           </tr>
         </thead>
-        
-        {/* <tr>
-          <td>
-            Giuliano
-          </td>
-          <td>
-            Serawop
-          </td>
-          <td>
-            Male
-          </td>
-          <td>
-            Vernal
-          </td>
-        </tr>  */}
-        {userInfo && userInfo.map((item) => {
-          <tbody>
-            <tr>
-              <td>{item.name.first}</td>
-              <td>{item.name.last}</td>
-              <td>{item.gender}</td>
-              <td>{item.city}</td>
-            </tr>
-          </tbody>
-        })}
+        <tbody>
+          {userInfo && userInfo.map((item, index) => {
+              return (<tr key={index}>
+                <td>{item.name.first}</td>
+                <td>{item.name.last}</td>
+                <td>{item.gender}</td>
+                <td>{item.city}</td>
+                <td>{item.email}</td>
+              </tr>);
+            }
+          )}
+        </tbody>
       </table>
     </div>
   );
